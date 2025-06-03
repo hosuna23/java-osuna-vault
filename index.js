d44ec644970c35daefbc9d0aebb8bd9fb2a4f05d
Osuna-HJ <hector.osuna@adiglobal.com>
2025-06-02 17:35:32 -0700

    added the js lines to get the combination on the console

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1 = 7 + 3;
+
+let code2 = 150 / 3 - 10; 
+
+let code3 = 139 % 100; 
+
+let message = `The vault has been secured. The combination is`; 
+console.log(message)
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+console.log(codeA)
+
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB)
+
